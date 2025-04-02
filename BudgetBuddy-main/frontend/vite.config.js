fe18919831080c1d89089f2d1097d9302105cb54
torisutansan02 <torisutansan02@gmail.com>
2025-04-02 00:12:12 -0700

    Making changes

--- BudgetBuddy-main/frontend/vite.config.js
+++ BudgetBuddy-main/frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'frontend/dist', // Ensure this matches with Vercel settings
+    outDir: 'public', // Ensure this matches with Vercel settings
     emptyOutDir: true,
   },
   server: {
