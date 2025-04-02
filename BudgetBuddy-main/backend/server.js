4bc6ec7110ce2a448bed382578f9de897e50aab2
torisutansan02 <torisutansan02@gmail.com>
2025-04-01 23:58:57 -0700

    Fixing

--- BudgetBuddy-main/backend/server.js
+++ BudgetBuddy-main/backend/server.js
@@ -81,7 +81,7 @@ mongoose.connect(process.env.MONGO_URI)
         if (process.env.NODE_ENV === 'production') {
             app.use(express.static(path.join(__dirname, '../frontend', 'build')));
             app.get('/*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../frontend', 'frontend/dist', 'index.html'));
+            res.sendFile(path.join(__dirname, '../frontend/frontend', 'dist', 'index.html'));
             })
         }
     })
