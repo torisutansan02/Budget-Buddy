046c90ae3517a477c075ba73b2b788fa89a6d18d
torisutansan02 <torisutansan02@gmail.com>
2025-04-02 13:58:36 -0700

    Making changes

--- BudgetBuddy-main/backend/server.js
+++ BudgetBuddy-main/backend/server.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(cors({
   origin:  [
+    'http://localhost:3000',
     'http://localhost:5173',
     'https://budget-buddy-dusky-sigma.vercel.app',
   ],
